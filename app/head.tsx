c8da20eaf656c757a14199cedabd16ae7c0fc751
Warren Gerald Polandra <warren.polandra@gmail.com>
2025-06-03 22:48:56 +0800

    [main] fix: fix dark mode issue

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const head = () => {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="color-scheme" content="only light" />
+      <meta name="supported-color-schemes" content="light" />
+    </>
+  );
+};
+
+export default head;
